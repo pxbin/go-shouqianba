7fade0cd569f292ef046dd6c0e3b3a3198829ccb
pxbin <genban316@gmail.com>
2024-03-18 14:07:23 +0800

    feat: 收钱吧跳转支付

--- gateway.go
+++ gateway.go
@@ -5,11 +5,10 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"net/http"
-	"net/url"
+	"sort"
 	"strings"
 
-	"github.com/google/go-querystring/query"
+	"github.com/mitchellh/mapstructure"
 )
 
 // GatewayService handles communication with the wap gateway related
@@ -20,64 +19,59 @@ type GatewayService service
 
 type GatewayRequest struct {
 	// AppID 收钱吧分配的APPID
-	TerminalSN   string `url:"terminal_sn" json:"terminal_sn"`               // 收钱吧终端ID
-	ClientSN     string `url:"client_sn" json:"client_sn"`                   // 商户系统订单号
-	TotalAmount  string `url:"total_amount" json:"total_amount"`             // 订单总金额
-	Subject      string `url:"subject" json:"subject"`                       // 交易概述
-	Payway       string `url:"payway" json:"payway,omitempty"`               // 支付方式
-	Operator     string `url:"operator" json:"operator,omitempty"`           // 门店操作员
-	Description  string `url:"description" json:"description,omitempty"`     // 商品详情
-	Longitude    string `url:"longitude" json:"longitude,omitempty"`         // 经度
-	Latitude     string `url:"latitude" json:"latitude,omitempty"`           // 纬度
-	Extended     string `url:"extended" json:"extended,omitempty"`           // 扩展参数集合
-	GoodsDetails string `url:"goods_details" json:"goods_details,omitempty"` // 商品详情
-	Reflect      string `url:"reflect" json:"reflect,omitempty"`             // 反射参数
-	NotifyURL    string `url:"notify_url" json:"notify_url,omitempty"`       // 服务器异步回调 url
-	ReturnURL    string `url:"return_url" json:"return_url"`                 // 页面跳转同步通知页面路径
+	TerminalSN   string `mapstructure:"terminal_sn" json:"terminal_sn"`                         // 收钱吧终端ID
+	ClientSN     string `mapstructure:"client_sn" json:"client_sn"`                             // 商户系统订单号
+	TotalAmount  string `mapstructure:"total_amount" json:"total_amount"`                       // 订单总金额
+	Subject      string `mapstructure:"subject" json:"subject"`                                 // 交易概述
+	Payway       string `mapstructure:"payway,omitempty" json:"payway,omitempty"`               // 支付方式
+	Operator     string `mapstructure:"operator" json:"operator,omitempty"`                     // 门店操作员
+	Description  string `mapstructure:"description,omitempty" json:"description,omitempty"`     // 商品详情
+	Longitude    string `mapstructure:"longitude,omitempty" json:"longitude,omitempty"`         // 经度
+	Latitude     string `mapstructure:"latitude,omitempty" json:"latitude,omitempty"`           // 纬度
+	Extended     string `mapstructure:"extended,omitempty" json:"extended,omitempty"`           // 扩展参数集合
+	GoodsDetails string `mapstructure:"goods_details,omitempty" json:"goods_details,omitempty"` // 商品详情
+	Reflect      string `mapstructure:"reflect,omitempty" json:"reflect,omitempty"`             // 反射参数
+	NotifyURL    string `mapstructure:"notify_url,omitempty" json:"notify_url,omitempty"`       // 服务器异步回调 url
+	ReturnURL    string `mapstructure:"return_url" json:"return_url"`                           // 页面跳转同步通知页面路径
 }
 
-func (r *GatewayRequest) signed(terminalKey string) (string, error) {
-	v, err := query.Values(r)
-	if err != nil {
-		return "", err
-	}
-	v.Del("sign")
-	v.Del("sign_type")
-
-	signstr := fmt.Sprintf("%s&key=%s", v.Encode(), terminalKey)
-	sum := md5.Sum([]byte(signstr))
-	signed := strings.ToUpper(hex.EncodeToString(sum[:]))
-	v.Add("sign", signed)
-	return v.Encode(), nil
-}
-
-// Request 收银吧网关支付 WAP支付收银台
+// GetWapURL 收银吧网关支付 WAP支付收银台
 // https://doc.shouqianba.com/zh-cn/api/wap2.html
-func (s *GatewayService) Request(ctx context.Context, req *GatewayRequest) (*http.Response, error) {
+func (s *GatewayService) GetWapURL(ctx context.Context, req *GatewayRequest) (string, error) {
 	req.TerminalSN = s.client.config.TerminalSN
 
 	if s.client.config.NotifyURL != "" {
 		req.NotifyURL = s.client.config.NotifyURL
 	}
-	if s.client.config.ReturnURL != "" {
-		req.ReturnURL = s.client.config.ReturnURL
-	}
+	params := map[string]string{}
+	mapstructure.Decode(req, &params)
 
-	u, err := url.Parse(apiGatewayBaseURL)
-	if err != nil {
-		return nil, err
-	}
+	q := encode(params)
 
-	sv, err := req.signed(s.client.config.TerminalKey)
-	if err != nil {
-		return nil, err
-	}
-	u.RawQuery = sv
+	signstr := fmt.Sprintf("%s&key=%s", q, s.client.config.TerminalKey)
+	sum := md5.Sum([]byte(signstr))
+	signed := strings.ToUpper(hex.EncodeToString(sum[:]))
+	return fmt.Sprintf("%s?%s&sign=%s", apiGatewayBaseURL, q, signed), nil
+}
 
-	resp, err := s.client.Request(ctx, http.MethodGet, u.String(), nil, nil)
-	if err != nil {
-		return resp, err
+func encode(v map[string]string) string {
+	if len(v) == 0 {
+		return ""
 	}
-
-	return resp, nil
+	var buf strings.Builder
+	keys := make([]string, 0, len(v))
+	for k := range v {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		v := v[k]
+		if buf.Len() > 0 {
+			buf.WriteByte('&')
+		}
+		buf.WriteString(k)
+		buf.WriteByte('=')
+		buf.WriteString(v)
+	}
+	return buf.String()
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.0
 require (
 	github.com/google/go-cmp v0.6.0
 	github.com/google/go-querystring v1.1.0
+	github.com/mitchellh/mapstructure v1.5.0
 )
