bfd7378c8c8fdeef3d5c81a2c23100833b094201
pxbin <genban316@gmail.com>
2024-03-21 21:46:45 +0800

    fix: 更新业务响应字段

--- types.go
+++ types.go
@@ -20,28 +20,28 @@ type (
 	// BizData 业务响应数据报文
 	// https://doc.shouqianba.com/zh-cn/api/annex/responseParams.html#biz_response.data内字段列表
 	BizData struct {
-		TerminalSN        string    `json:"terminal_sn,omitempty"`
-		SN                string    `json:"sn,omitempty"`
-		ClientSN          string    `json:"client_sn,omitempty"`
-		Status            string    `json:"status,omitempty"`
-		OrderStatus       string    `json:"order_status,omitempty"`
-		PayerUID          string    `json:"payer_uid,omitempty"`
-		PayerLogin        string    `json:"payer_login,omitempty"`
-		TradeNo           string    `json:"trade_no,omitempty"`
-		QrCode            string    `json:"qr_code,omitempty"`
-		TotalAmount       int64     `json:"total_amount,string,omitempty"`
-		NetAmount         int64     `json:"net_amount,string,omitempty"`
-		Payway            string    `json:"payway,omitempty"`
-		PaywayName        string    `json:"payway_name,omitempty"`
-		SubPayway         string    `json:"sub_payway,omitempty"`
-		FinishTime        Timestamp `json:"finish_time,omitempty"`
-		ChannelFinishTime Timestamp `json:"channel_finish_time,omitempty"`
-		StoreID           string    `json:"store_id,omitempty"`
-		Subject           string    `json:"subject,omitempty"`
-		Description       string    `json:"description,omitempty"`
-		Reflect           string    `json:"reflect,omitempty"`
-		Operator          string    `json:"operator,omitempty"`
-		WapPayRequest     string    `json:"wap_pay_request,omitempty"`
+		TerminalSN        string `json:"terminal_sn,omitempty"`
+		SN                string `json:"sn,omitempty"`
+		ClientSN          string `json:"client_sn,omitempty"`
+		Status            string `json:"status,omitempty"`
+		OrderStatus       string `json:"order_status,omitempty"`
+		PayerUID          string `json:"payer_uid,omitempty"`
+		PayerLogin        string `json:"payer_login,omitempty"`
+		TradeNo           string `json:"trade_no,omitempty"`
+		QrCode            string `json:"qr_code,omitempty"`
+		TotalAmount       int64  `json:"total_amount,string,omitempty"`
+		NetAmount         int64  `json:"net_amount,string,omitempty"`
+		Payway            string `json:"payway,omitempty"`
+		PaywayName        string `json:"payway_name,omitempty"`
+		SubPayway         string `json:"sub_payway,omitempty"`
+		FinishTime        int64  `json:"finish_time,omitempty"`
+		ChannelFinishTime int64  `json:"channel_finish_time,omitempty"`
+		StoreID           string `json:"store_id,omitempty"`
+		Subject           string `json:"subject,omitempty"`
+		Description       string `json:"description,omitempty"`
+		Reflect           string `json:"reflect,omitempty"`
+		Operator          string `json:"operator,omitempty"`
+		WapPayRequest     string `json:"wap_pay_request,omitempty"`
 	}
 
 	// GoodDetail represents the goods detail for payment
